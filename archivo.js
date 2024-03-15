1bdd51774fb5ff207581a4d7beeeec5a1d4ef9cd
Tami <tamisimes2@gmail.com>
2024-03-15 15:54:16 -0300

    animaciones y form

--- archivo.js
+++ archivo.js
@@ -1,57 +1,15 @@
-$(document).ready(function() {
-    function animateCartas(cartas) {
-        cartas.each(function(index) {
-            var carta = $(this);
-            setTimeout(function() {
-                carta.addClass('mostrar');
-            }, 300 * index);
-        });
-    }
+  document.addEventListener('DOMContentLoaded', function () {
+    const botonSayHello = document.getElementById('boton-say-hello');
+    const contactForm = document.getElementById('contactForm');
+    const overlay = document.getElementById('overlay');
 
-    
-
-    function checkVisibility() {
-        $('.seccion2 ').each(function() {
-            if (elementInViewport(this) && !$(this).hasClass('mostrar')) {
-                $(this).addClass('mostrar');
-                animateCartas($(this).find('.carta-experiencia'));
-            }
-        });
-
-        $('.carta-experiencia', ).each(function(index) {
-            if (elementInViewport(this) && !$(this).hasClass('mostrar')) {
-                var carta = $(this);
-                setTimeout(function() {
-                    carta.addClass('mostrar');
-                }, 300 * index);
-            }
-        });
-    }
-
-    function elementInViewport(el) {
-        var rect = el.getBoundingClientRect();
-        return (
-            rect.top >= 0 &&
-            rect.left >= 0 &&
-            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-        );
-    }
-
-    checkVisibility();
-
-    $(window).on('scroll resize', function() {
-        checkVisibility();
+    botonSayHello.addEventListener('click', function () {
+      contactForm.style.display = 'block';
+      overlay.style.display = 'block';
     });
 
-});
-
-document.getElementById('contactBtn').addEventListener('click', function() {
-    var form = document.getElementById('contactForm');
-    if (form.style.display === 'none') {
-      form.style.display = 'block';
-    } else {
-      form.style.display = 'none';
-    }
+    overlay.addEventListener('click', function () {
+      contactForm.style.display = 'none';
+      overlay.style.display = 'none';
+    });
   });
-  
